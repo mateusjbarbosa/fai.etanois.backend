8feee686a20c70a990467cd9c724583076009ccb
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2020-02-25 08:42:32 -0300

    Adição do gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var ts = require('gulp-typescript');
+
+var tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('clean', function() {
+  return gulp
+    .src('dist', { allowEmpty: true })
+    .pipe(clean());
+});
+
+gulp.task('compile', gulp.series('clean', function() {
+  return tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('dist'))
+}));
+
+gulp.task('default', gulp.series('compile'));
