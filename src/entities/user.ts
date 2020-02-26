1e6e0519578d156c74795e5f0fd94cd9bd456001
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2020-02-25 21:20:33 -0300

    Criação da entidade user no banco
    #2

---
+++ src/entities/user.ts
@@ -0,0 +1,84 @@
+import * as bcrypt from 'bcrypt';
+
+export default function (sequelize, DataTypes) {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    phone_number: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+        notEmpty: true,
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [6, 50],
+        notNull: {
+          msg: 'Username is required'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [6, 50],
+        notNull: {
+          msg: 'Password is required'
+        }
+      }
+    },
+    cep: {
+      type: DataTypes.STRING(8),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [8, 8]
+      }
+    },
+    payment_mode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      },
+      isIn: 
+      {
+        args: [['money', 'credit_car', 'both']],
+        msg: 'Invalid payment format'
+      }
+    }
+  });
+
+  User.beforeCreate((user) => {
+    return hashPassword(user);
+  });
+
+  User.beforeUpdate((user) => {
+    return hashPassword(user);
+  });
+  
+  function hashPassword(user) {
+    const salt = bcrypt.genSaltSync(10);
+    user.set('password', bcrypt.hashSync(user.password, salt));
+  }
+
+  return User;
+}
