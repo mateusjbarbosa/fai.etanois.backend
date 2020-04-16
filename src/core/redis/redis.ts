bd8895f5f0c41d488c9b4083e761b5cda6771b2a
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2020-04-15 22:48:48 -0300

    Início da comunicação com o Redis

---
+++ src/core/redis/redis.ts
@@ -0,0 +1,38 @@
+import * as redis from 'redis'
+import Configuration from '../../config/config'
+import { token } from 'morgan'
+
+class Redis {
+  private configRedis: redis.ClientOpts
+  private client: redis.RedisClient
+
+  constructor() {
+    this.configRedis = {
+      host: Configuration.redis.host,
+      port: Configuration.redis.port
+    }
+
+    this.client = redis.createClient(this.configRedis)
+    this.client.on('connect', () => {})
+    this.client.on('error', (e) => {
+      console.log('Error connecting Redis!!!')
+    })
+  }
+
+  public createRecoverPassword(token: string, userId: string) {
+    this.client.set(token, userId, () => {})
+    this.client.expire(token, (60), () => {})
+  }
+
+  public async verifyExistenceToken(token: string): Promise<string> {
+    return new Promise(resolve => {
+      this.client.mget(token, (err, result) => {
+        this.client.del(token)
+        
+        resolve(result[0])
+      });
+    })
+  }
+}
+
+export default new Redis()
