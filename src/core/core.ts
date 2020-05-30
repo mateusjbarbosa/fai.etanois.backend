970213f8c5c26b30d774694fd83321bf31ecb558
João Vitor Teixeira <jvitor@pdsolucoes.com.br>
2020-05-29 21:26:34 -0300

    Remoção de código não mais utilizado

--- src/core/core.ts
+++ src/core/core.ts
@@ -20,7 +20,6 @@ export class CoreModule {
   }
 
   private configExpress(): void {
-    this.express.use(this.configHeaders.bind(this));
     this.express.use(morgan('dev'));
     this.express.use(bodyParser.urlencoded({ extended: true }));
     this.express.use(bodyParser.json());
@@ -35,12 +34,5 @@ export class CoreModule {
 
   private router(): void {
     this.routerModule.exposeRoutes(AuthService.authenticate);
-  }
-
-  private configHeaders(req: Request, res: Response, next: NextFunction) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    next();
   }
 }
