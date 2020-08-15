a78b8057ced3ac4724d6680d794012417d303418
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2020-08-15 15:53:11 -0300

    Melhoria na rotina util

--- src/core/util/util.ts
+++ src/core/util/util.ts
@@ -17,11 +17,14 @@ export const to = <T>(promise: Promise<T>): Promise<[Error | undefined, T]> =>
     .catch<[Error, any]>((err: Error) => [err, undefined]);
 
 export function findWithAttr(array: any[], attr: string, value: any): number {
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i][attr] === value) {
-      return i;
+  if (Array.isArray(array)) {
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i][attr] == value) {
+        return i;
+      }
     }
   }
+
   return -1;
 }
 
