06a70649d2a4efe9a700d79e0619767a284da36e
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2020-09-07 11:28:21 -0300

    Continuação de melhorias no log

--- src/core/router/routes.ts
+++ src/core/router/routes.ts
@@ -1,4 +1,4 @@
-import { Application} from 'express';
+import { Application, Request, Response, NextFunction } from 'express';
 import { RouterModuleFactory} from './router-map';
 import { HttpVerbMap, FeatureModuleRouterInfo, ModuleEndpointMap } from './base-routes-module';
 
@@ -43,16 +43,21 @@ export class RouterModule {
       if(routerInfo) {
         const {isProtected, callback, endpoint} = routerInfo;
         isProtected 
-          ? this.express.route(endpoint).all(authenticate())[resgistredVeb](function (req, res, next) {
-            console.log(`\nEndpoint: ${endpoint}\n`);
-            next();
-          },callback)
-          : this.express.route(endpoint)[resgistredVeb](function (req, res, next) {
-            console.log(`\nEndpoint: ${endpoint}\n`);
-            next();
-          }, callback);
+          ? this.express.route(endpoint).all(authenticate())[resgistredVeb]
+            (this.printRequestRoute ,callback)
+          : this.express.route(endpoint)[resgistredVeb](this.printRequestRoute, callback);
 
         console.log(`${resgistredVeb}: ${endpoint} | isProtected: ${isProtected}`);
       }
+  }
+
+  private printRequestRoute(req: Request, res: Response, next: NextFunction): void {
+    console.log(`\nURL:${req.url}`)
+
+    if (req.user) {
+      console.log(`User: ${req.user[`username`]}`)
+    }
+
+    next();
   }
 }
